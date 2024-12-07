59ad2f57f2b8475bb56818af50002c6b67176f5c
Ab <52878876+Dulchik@users.noreply.github.com>
2024-12-07 10:03:32 +0100

    Fuck this shit, it's not working

--- script.py
+++ script.py
@@ -69,4 +69,4 @@ hours = total_seconds // 3600
 minutes = (total_seconds % 3600) // 60
 seconds = total_seconds % 60
 
-print(f"Total Playlist Duration: {hours} hours, {minutes} minutes, {seconds} seconds")
+print(f"Total Playlist Duration: {hours} hours, {minutes} minutes, {seconds} seconds.")
