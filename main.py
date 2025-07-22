e9872e5cf5c73c474e857c6c314f6f43ebaa389c
dulchik <bubnyukab@gmail.com>
2025-07-22 12:06:36 +0200

    fetched all video ids

--- main.py
+++ main.py
@@ -1,9 +1,56 @@
-import requests
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.playlistItems.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/code-samples#python
+
+import os
+from dotenv import load_dotenv
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+load_dotenv()
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 def main():
-    pass
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = os.getenv("CLIENT_SECRET")
 
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_local_server()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
 
-LINK = "https://www.youtube.com/playlist?list=WL"
+    video_ids = []
+    next_page_token = None
 
+    while True:
+        request = youtube.playlistItems().list(
+            part="contentDetails",
+            maxResults=50,
+            playlistId="PLXZBc7tmmYhdP6O8PJH1yZ6wepAAKCNrD",
+            pageToken=next_page_token
+        )
+        response = request.execute()
+
+        for item in response["items"]:
+            video_ids.append(item["contentDetails"]["videoId"])
+        
+        next_page_token = response.get("nextPageToken")
+        if not next_page_token:
+            break
+
+    print(f"Total video IDs fetched: {len(video_ids)}")
+
+if __name__ == "__main__":
+    main()
