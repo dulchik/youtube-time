77168f13b1d05eec35c44567b64f9f27ab0f74b1
dulchik <bubnyukab@gmail.com>
2025-07-23 10:34:05 +0200

    remove the oauth part, cleaned up some files

--- main.py
+++ main.py
@@ -1,51 +1,30 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.playlistItems.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
-
 import os
 from dotenv import load_dotenv
 import isodate
-
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
-import googleapiclient.errors
 
 load_dotenv()
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+API_KEY = os.getenv("API_KEY")
 
 def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = os.getenv("CLIENT_SECRET")
-    API_KEY = os.getenv("API_KEY")
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_local_server()
-    youtube_auth = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials
-    )
-
-    youtube_key = googleapiclient.discovery.build(
+    # Get API key 
+    youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=API_KEY
     )
-
+    
+    playlist_id = "PLXZBc7tmmYhdP6O8PJH1yZ6wepAAKCNrD"
     video_ids = []
     next_page_token = None
 
     while True:
-        request = youtube_auth.playlistItems().list(
+        request = youtube.playlistItems().list(
             part="contentDetails",
             maxResults=50,
-            playlistId="PLXZBc7tmmYhdP6O8PJH1yZ6wepAAKCNrD",
+            playlistId=playlist_id,
             pageToken=next_page_token
         )
         response = request.execute()
@@ -62,7 +41,7 @@ def main():
     durations = []
 
     for video_id in video_ids:
-        request = youtube_key.videos().list(
+        request = youtube.videos().list(
             part="contentDetails",
             id=video_id
         )
@@ -77,7 +56,7 @@ def main():
     minutes = int((total_seconds % 3600) // 60)
     seconds = int(total_seconds % 60)
 
-    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    print(f"Total time needed to finish the playlist: {hours:02}:{minutes:02}:{seconds:02}")
 
 if __name__ == "__main__":
     main()
